ebb946db7ee747047f36ddac9fe72544e73b3192
Yoon Ho Han <yhhan@ucsd.edu>
2015-05-18 16:49:56 -0700

    updated author and email

--- solutions/p2_is_consistent.py
+++ solutions/p2_is_consistent.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-__author__ = 'Please write your names, separated by commas.'
-__email__ = 'Please write your email addresses, separated by commas.'
+__author__ = 'Yoon Ho Han, Kevin Zhang'
+__email__ = 'yhhan@ucsd.edu, ktzhang@ucsd.edu'
 
 
 def is_consistent(csp, variable, value):
